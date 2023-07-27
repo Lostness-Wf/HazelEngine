bd9cef0260f203d807632de87682cb7bbaab7ca0
weifuchunyang <1306701599@qq.com>
2023-07-27 13:10:58 +0800

    add pch header

---
+++ Hazel/src/hzpch.h
@@ -0,0 +1,19 @@
+#pragma once
+
+//Ô¤±àÒëÍ·ÎÄ¼þ
+
+#include<iostream>
+#include<memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#ifdef HZ_PLATFORM_WINDOWS
+	#include <windows.h>
+#endif // HZ_PLATFORM_WINDOWS
