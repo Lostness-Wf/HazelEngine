5feb1e1ffe9adcf3f6d682bcc2c6f7dc5a617a1d
weifuchunyang <1306701599@qq.com>
2023-08-02 17:11:57 +0800

    vertex buffer layout(auto calculate offsets and Stride)

--- Hazel/src/Hazel/Renderer/Buffer.h
+++ Hazel/src/Hazel/Renderer/Buffer.h
@@ -1,14 +1,119 @@
 #pragma once
 
 namespace Hazel {
+	enum class ShaderDataType
+	{
+		None = 0, Float, Float2, Float3, Float4, Mat3, Mat4, Int, Int2, Int3, Int4, Bool
+	};
+
+	static uint32_t ShaderDataTypeSize(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case ShaderDataType::Float:    return 4;
+		case ShaderDataType::Float2:   return 4 * 2;
+		case ShaderDataType::Float3:   return 4 * 3;
+		case ShaderDataType::Float4:   return 4 * 4;
+		case ShaderDataType::Mat3:     return 4 * 3 * 3;
+		case ShaderDataType::Mat4:     return 4 * 4 * 4;
+		case ShaderDataType::Int:      return 4;
+		case ShaderDataType::Int2:     return 4 * 2;
+		case ShaderDataType::Int3:     return 4 * 3;
+		case ShaderDataType::Int4:     return 4 * 4;
+		case ShaderDataType::Bool:     return 1;
+		}
+
+		HZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
+	struct BufferElement
+	{
+		std::string Name;
+		ShaderDataType Type;
+		uint32_t Size;
+		uint32_t Offset;
+		bool Normalized;
+
+		BufferElement() {}
+
+		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false)
+			: Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0), Normalized(normalized)
+		{
+		}
+
+		uint32_t GetComponentCount() const
+		{
+			switch (Type)
+			{
+			case ShaderDataType::Float:   return 1;
+			case ShaderDataType::Float2:  return 2;
+			case ShaderDataType::Float3:  return 3;
+			case ShaderDataType::Float4:  return 4;
+			case ShaderDataType::Mat3:    return 3 * 3;
+			case ShaderDataType::Mat4:    return 4 * 4;
+			case ShaderDataType::Int:     return 1;
+			case ShaderDataType::Int2:    return 2;
+			case ShaderDataType::Int3:    return 3;
+			case ShaderDataType::Int4:    return 4;
+			case ShaderDataType::Bool:    return 1;
+			}
+
+			HZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
+			return 0;
+		}
+	};
+
+	class BufferLayout
+	{
+	public:
+		BufferLayout() {}
+
+		BufferLayout(const std::initializer_list<BufferElement>& elements)
+			: m_Elements(elements)
+		{
+			CalculateOffsetsAndStride();
+		}
+
+		inline uint32_t GetStride() const { return m_Stride; }
+		inline const std::vector<BufferElement>& GetElements() const { return m_Elements; }
+
+		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
+		std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
+		//const_iterator 是指向 const 元素的迭代器，因此，虽然迭代器本身可以更新（例如，递增或递减），但是指向的元素不能更改。
+		std::vector<BufferElement>::const_iterator begin() const { return m_Elements.begin(); }
+		std::vector<BufferElement>::const_iterator end() const { return m_Elements.end(); }
+	private:
+		void CalculateOffsetsAndStride()
+		{
+			uint32_t offset = 0;
+			m_Stride = 0;
+			for (auto& element : m_Elements)
+			{
+				element.Offset = offset;
+				offset += element.Size;
+				m_Stride += element.Size;
+			}
+		}
+	private:
+		std::vector<BufferElement> m_Elements;
+		uint32_t m_Stride = 0;
+	};
 
 	class VertexBuffer
 	{
 	public:
+		/*把析构函数标记成virtual的目的是为了在用基类指针删除派生类对象时，能够正确调用派生类的析构函数，释放资源。
+		如果基类的析构函数不是虚函数，那么只会调用基类的析构函数，可能造成内存泄漏。当然，如果你的类没有其他虚函数，
+		也没有指针成员，那么你可以不用把析构函数声明为virtual。*/
 		virtual ~VertexBuffer() {}
 
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
+
+		virtual const BufferLayout& GetLayout() const = 0;
+		virtual void SetLayout(const BufferLayout& layout) = 0;
+
 
 		static VertexBuffer* Create(float* vertices, uint32_t size);
 	};
