590fc138016977d0b3de679b2683327e6da29ec2
weifuchunyang <1306701599@qq.com>
2024-01-28 01:03:52 +0800

    Add: Create Renderer2D

--- Hazel/src/Hazel/Renderer/Renderer.cpp
+++ Hazel/src/Hazel/Renderer/Renderer.cpp
@@ -2,6 +2,7 @@
 #include "Renderer.h"
 
 #include <Platform/OpenGL/OpenGLShader.h>
+#include "Renderer2D.h"
 
 namespace Hazel {
 	Renderer::SceneData* Renderer::m_SceneData = new Renderer::SceneData;
@@ -9,6 +10,7 @@ namespace Hazel {
 	void Renderer::Init()
 	{
 		RenderCommand::Init();
+		Renderer2D::Init();
 	}
 
 	void Renderer::OnWindowResize(uint32_t width, uint32_t height)
