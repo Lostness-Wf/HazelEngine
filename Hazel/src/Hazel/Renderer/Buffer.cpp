1842ba015cbba6c8b0462fcfffd830dbfdffd3bb
weifuchunyang <1306701599@qq.com>
2024-01-30 00:42:56 +0800

    Fix: code maintenance, fix wornings

--- Hazel/src/Hazel/Renderer/Buffer.cpp
+++ Hazel/src/Hazel/Renderer/Buffer.cpp
@@ -9,26 +9,22 @@ namespace Hazel {
 
 	Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size)
 	{
-		//如为OpenGLAPI，新建一个vertexbuffer
 		switch (Renderer::GetAPI())
 		{
-			case RendererAPI::API::None:    HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-			case RendererAPI::API::OpenGL:  return CreateRef<OpenGLVertexBuffer>(vertices, size);
+		case RendererAPI::API::None:    HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return CreateRef<OpenGLVertexBuffer>(vertices, size);
 		}
 
 		HZ_CORE_ASSERT(false, "Unknown RendererAPI!");
 		return nullptr;
 	}
 
-
-
 	Ref<IndexBuffer> IndexBuffer::Create(uint32_t* indices, uint32_t size)
 	{
-		//如为OpenGLAPI，新建一个indexbuffer
 		switch (Renderer::GetAPI())
 		{
-			case RendererAPI::API::None:    HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-			case RendererAPI::API::OpenGL:  return CreateRef<OpenGLIndexBuffer>(indices, size);
+		case RendererAPI::API::None:    HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return CreateRef<OpenGLIndexBuffer>(indices, size);
 		}
 
 		HZ_CORE_ASSERT(false, "Unknown RendererAPI!");
--- Hazel/src/Hazel/Renderer/Buffer.h
+++ Hazel/src/Hazel/Renderer/Buffer.h
@@ -1,6 +1,7 @@
-#pragma once////////////////////////////////////////////////////////////////////
+#pragma once
 
 namespace Hazel {
+
 	enum class ShaderDataType
 	{
 		None = 0, Float, Float2, Float3, Float4, Mat3, Mat4, Int, Int2, Int3, Int4, Bool
@@ -32,10 +33,10 @@ namespace Hazel {
 		std::string Name;
 		ShaderDataType Type;
 		uint32_t Size;
-		uint32_t Offset;
+		size_t Offset;
 		bool Normalized;
 
-		BufferElement() {}
+		BufferElement() = default;
 
 		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false)
 			: Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0), Normalized(normalized)
@@ -69,10 +70,6 @@ namespace Hazel {
 	public:
 		BufferLayout() {}
 
-		/*std::initializer_list 是一个轻量级的代理对象，它提供对类型为 const T 的对象数组的访问。
-		当使用花括号初始化列表来初始化一个对象时，会自动构造一个 std::initializer_list 对象。
-		代理对象是一种设计模式，它提供了对目标对象的另外一种访问方式。通过代理对象访问目标对象，
-		可以在目标对象实现的基础上增强额外的功能操作，即扩展目标对象的功能。*/
 		BufferLayout(const std::initializer_list<BufferElement>& elements)
 			: m_Elements(elements)
 		{
@@ -84,13 +81,12 @@ namespace Hazel {
 
 		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
 		std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
-		//const_iterator 是指向 const 元素的迭代器，因此，虽然迭代器本身可以更新（例如，递增或递减），但是指向的元素不能更改。
 		std::vector<BufferElement>::const_iterator begin() const { return m_Elements.begin(); }
 		std::vector<BufferElement>::const_iterator end() const { return m_Elements.end(); }
 	private:
 		void CalculateOffsetsAndStride()
 		{
-			uint32_t offset = 0;
+			size_t offset = 0;
 			m_Stride = 0;
 			for (auto& element : m_Elements)
 			{
@@ -107,9 +103,6 @@ namespace Hazel {
 	class VertexBuffer
 	{
 	public:
-		/*把析构函数标记成virtual的目的是为了在用基类指针删除派生类对象时，能够正确调用派生类的析构函数，释放资源。
-		如果基类的析构函数不是虚函数，那么只会调用基类的析构函数，可能造成内存泄漏。当然，如果你的类没有其他虚函数，
-		也没有指针成员，那么你可以不用把析构函数声明为virtual。*/
 		virtual ~VertexBuffer() = default;
 
 		virtual void Bind() const = 0;
@@ -117,7 +110,6 @@ namespace Hazel {
 
 		virtual const BufferLayout& GetLayout() const = 0;
 		virtual void SetLayout(const BufferLayout& layout) = 0;
-
 
 		static Ref<VertexBuffer> Create(float* vertices, uint32_t size);
 	};
