f83fa794dda991dfa2cb9ab30fddd1ef39d700ca
weifuchunyang <1306701599@qq.com>
2024-02-06 14:04:59 +0800

    Change: Update EditorLayer

--- Hazel-Editor/src/EditorLayer.cpp
+++ Hazel-Editor/src/EditorLayer.cpp
@@ -162,6 +162,7 @@ namespace Hazel {
 		ImGui::Image((void*)textureID, ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
 		ImGui::End();
 		ImGui::PopStyleVar();
+
 		ImGui::End();
 	}
 
