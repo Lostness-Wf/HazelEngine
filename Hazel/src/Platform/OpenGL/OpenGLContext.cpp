6e18ec7bdb1eaeab06ed16336f16c047cd37b7e6
weifuchunyang <1306701599@qq.com>
2024-02-11 00:55:03 +0800

    Revert "Fix:"
    This reverts commit d84b948a68f656bfdb3885a34556f89332cdeb40.

--- Hazel/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLContext.cpp
@@ -24,9 +24,9 @@ namespace Hazel {
 		
 		//GPU Version
 		HZ_CORE_INFO("OpenGL Info:");
-		HZ_CORE_INFO("  Vendor: {0}", (const char*)glGetString(GL_VENDOR));
-		HZ_CORE_INFO("  Renderer: {0}", (const char*)glGetString(GL_RENDERER));
-		HZ_CORE_INFO("  Version: {0}", (const char*)glGetString(GL_VERSION));
+		HZ_CORE_INFO("  Vendor: {0}", (const void*)glGetString(GL_VENDOR));
+		HZ_CORE_INFO("  Renderer: {0}", (const void*)glGetString(GL_RENDERER));
+		HZ_CORE_INFO("  Version: {0}", (const void*)glGetString(GL_VERSION));
 
 		HZ_CORE_ASSERT(GLVersion.major > 4 || (GLVersion.major == 4 && GLVersion.minor >= 5), "Hazel requires at least OpenGL version 4.5!");
 	}
