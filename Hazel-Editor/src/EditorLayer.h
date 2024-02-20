3d816505517f36b1f004531230b157c64242a692
Wei Fuchunyang <1306701599@qq.com>
2024-02-20 15:50:58 +0800

    Add: Visualize collision box

--- Hazel-Editor/src/EditorLayer.h
+++ Hazel-Editor/src/EditorLayer.h
@@ -24,6 +24,8 @@ namespace Hazel {
 		bool OnKeyPressed(KeyPressedEvent& e);
 		bool OnMouseButtonPressed(MouseButtonPressedEvent& e);
 
+		void OnOverlayRender();
+
 		void NewScene();
 		void OpenScene();
 		void OpenScene(const std::filesystem::path& path);
@@ -71,6 +73,8 @@ namespace Hazel {
 		glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 
 		int m_GizmoType = -1;
+
+		bool m_ShowPhysicsColliders = false;
 
 		enum class SceneState
 		{
