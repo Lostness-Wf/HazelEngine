13c2b90b9d429f28c1717be53e5a8c2c13a9ce11
Wei Fuchunyang <1306701599@qq.com>
2024-03-03 20:39:12 +0800

    Fix: Fixed some bugs
    -If Hazel-Editor has not a open project passed by command line, will open filedialog to select
    -Fix crash in release mode
    -Add UI class so we don't have to ImGui::PushColor and PopColor

--- Hazel/src/Hazel/Scripting/ScriptEngine.cpp
+++ Hazel/src/Hazel/Scripting/ScriptEngine.cpp
@@ -133,7 +133,12 @@ namespace Hazel {
 		Scope<filewatch::FileWatch<std::string>> AppAssemblyFileWatcher;
 		bool AssemblyReloadPending = false;
 
+#ifdef HZ_DEBUG
 		bool EnableDebugging = true;
+
+#else
+		bool EnableDebugging = false;
+#endif
 
 		// Runtime
 
